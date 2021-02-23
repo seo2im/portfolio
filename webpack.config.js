f803945c7dcaa423e09c08a26c34ddf5f4049714
seo1im <seolim6492@gmail.com>
2021-02-23 19:03:44 +0900

    publishing test

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,8 @@ module.exports = {
 
 	output : {
 		filename : '[name].bundle.js',
-		path : path.resolve(__dirname, './dist'),
-		publicPath : "/",
+		path : path.resolve(__dirname, './docs'),
+		
 	},
 	resolve : {
 		extensions : ['.js','.ts', '.tsx']
