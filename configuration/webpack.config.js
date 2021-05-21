ecb8ed7448cac5cba881cd0c823bb83b877f6988
seo2im <seo2im6492@gmail.com>
2021-05-21 22:05:17 +0900

    chore:set basic build file - html template - basic root file

---
+++ configuration/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './srcs/app.tsx',
+    target: 'web',
+    mode: 'development',
+
+    output: {
+        filename: '[name].bundle.js',
+        path: path.join(__dirname, '/build'),
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+            },
+            {
+                test: /\.png$/,
+                loader: 'url-loader'
+            }
+        ]
+    },
+    devServer : {
+        historyApiFallback : true,
+		port : 4000,
+	},
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../public', 'index.html')
+        })
+    ],
+}
---
+++ srcs/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App: React.FC = () => {
+    return (
+        <div>
+            APP
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
