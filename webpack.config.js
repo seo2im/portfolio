57568e8573ae723f59b1536e45219f663d539893
seo2im <seo2im6492@gmail.com>
2021-05-22 12:05:41 +0900

    feat:set router => hook set components - Router is redirection so it is not suitable clean animation

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
 	},
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../public', 'index.html')
+            template: path.resolve(__dirname, './public', 'index.html')
         })
     ],
 }
