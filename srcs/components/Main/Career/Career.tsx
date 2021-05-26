6d17a00d36280905e75d01e73a4328f4ea1cc5af
seo2im <seo2im6492@gmail.com>
2021-05-26 21:11:55 +0900

    feat:set main code style

--- srcs/components/Main/Career/Career.tsx
+++ srcs/components/Main/Career/Career.tsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import * as career from './Career.style'
+import { Postit, DragItem } from '../..'
+import config from '../../../../configuration'
+import { Title } from '../../'
 
-type Props = {
-    careers: {
-        date: string
-        content: string
-    }[]
-}
-const Career: React.FC<Props> = ({ careers }) => {
+const Career: React.FC = () => {
+    const { careers, title, postId } = config.main.career
+
     return (
-        <career.Layout>
-            {careers.map((car, i) => (
-                <career.Columm key={`career_${i}`}>
-                    <career.Duration>{car.date}</career.Duration>
-                    <career.Content>{car.content}</career.Content>
-                </career.Columm>
-            ))}
-        </career.Layout>
+        <DragItem initTop={0} initLeft={0} offsetX={300}>
+            <Postit width={500} height={500} postId={postId}>
+                <Title size={"large"} title={title} />
+                <career.Layout>
+                {careers.map((car, i) => (
+                    <career.Columm key={`career_${i}`}>
+                        <career.Duration>{car.date}</career.Duration>
+                        <career.Content>{car.content}</career.Content>
+                    </career.Columm>
+                ))}
+                </career.Layout>
+            </Postit>
+        </DragItem>
+        
+        
     )
 }
 
